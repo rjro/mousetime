0329de5d8f3e4719c5d591b6bc5f5868a2a698c3
rjro <rjro@me.com>
2020-06-14 01:00:11 -0700

    add times

--- itinerary.py
+++ itinerary.py
@@ -5,12 +5,12 @@ import gc
 
 gc.disable()
 
-data = json.load(open("fake_times.json"))
-rides = sorted(data[str(0)].keys())
-n = len(rides)
+data = json.load(open("times.json"))
+ride_names = sorted(data[str(495)].keys())
+n = len(ride_names)
 
 def getVisitedArray():
-    return [False for _ in range(len(rides))]
+    return [False for _ in range(n)]
 
 class Event(Enum):
     ATTRACTION = "ATTRACTION"
@@ -28,15 +28,16 @@ class ItineraryNode:
         self.prev = None
 
         self.root = 55
-        self.totalTime = 0
+        self.endTime = self.time + self.duration
 
     def __str__(self):
         if self.rideId != None:
-            return "%s -- %s -- %s -- %s -- %s" % (rides[self.rideId], self.eventType.value, self.time, self.duration, self.totalTime)
+            return "%s -- %s -- %s -- %s -- %s" % (ride_names[self.rideId], self.eventType.value, self.time, self.duration, self.endTime)
         else:
-            return "%s -- %s -- %s -- %s" % (self.eventType.value, self.time, self.duration, self.totalTime)
+            return "%s -- %s -- %s -- %s" % (self.eventType.value, self.time, self.duration, self.endTime)
 
-
+shortest_itinerary_seen = None
+earliest_time = float('inf')
 
 def printItinerary(itin):
 
@@ -51,18 +52,51 @@ def printItinerary(itin):
         lagger = lagger.next
 
 
-
+executions = 0
 
 def rideRides(itinerary, visited, time, depth):
-    
+    global earliest_time, executions
+
+    if executions % (10**5) == 0: print ("Executions:", executions)
+    executions += 1
+
+    '''
+    if depth == 5:
+        if itinerary.endTime < earliest_time:
+            earliest_time = itinerary.endTime
+            shortest_itinerary_seen = itinerary
+            printItinerary(itinerary)
+            print("NEW END TIME:", earliest_time)
+            print("\n\n\n---------")
+        return
+    '''
+    if depth == n:
+        if itinerary.endTime < earliest_time:
+            earliest_time = itinerary.endTime
+            shortest_itinerary_seen = itinerary
+            printItinerary(itinerary)
+            print("NEW END TIME:", earliest_time)
+            print("\n\n\n---------")
+        return
+
+    '''
     if all(visited):
         printItinerary(itinerary)
         print("\n\n\n---------")
         return
+    '''
 
     for i in range(n):
         if visited[i] == False:
-            ride_duration = 5
+            nearest_15_min_interval = 15 * (time//15)
+
+            current_wait_times =data[str(nearest_15_min_interval)]
+            ride_name = ride_names[i]
+
+            if ride_name not in current_wait_times:
+                return
+
+            ride_duration = current_wait_times[ride_name]
 
             ride_node = ItineraryNode(
                 time + ride_duration,
@@ -88,4 +122,4 @@ rideRides(ItineraryNode(
     0,
     0,
     Event.WAITING
-), getVisitedArray(), 0, 0)
+), visited=getVisitedArray(), time=495, depth=0)
