df6fc2df85e9267d2dc0ff41df4991e494991b6e
rjro <rjro@me.com>
2020-06-13 21:15:57 -0700

    clean up repo

--- itinerary.py
+++ itinerary.py
@@ -1,3 +1,15 @@
 import json
 
-data = json.open("")
+data = json.load(open("fake_times.json"))
+
+rides = sorted(data[str(0)].keys())
+
+def visitedArray():
+    return [False for _ in range(len(rides))]
+
+
+def rideRides(itinerary, visited, time, depth):
+    pass
+
+
+
